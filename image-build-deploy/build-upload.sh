a32a3dc7a83058a6e44bfafe3226a36f49d4e19f
Aneesh Bhat <imaneeshbhat@gmail.com>
2022-05-29 20:31:46 +0530

    change container repo name

--- image-build-deploy/build-upload.sh
+++ image-build-deploy/build-upload.sh
@@ -2,7 +2,7 @@
 sudo docker build -t basic-server -f image-build-deploy/DockerfileServer .
 
 # tag the image accordingly
-docker image tag basic-server aneeshbhat/basic-app/basic-server:latest
+docker image tag basic-server aneeshbhat/basic-server:latest
 
 # push the image to dockerhub account 'aneeshbhat' and repo 'basic-app'
-docker image push aneeshbhat/basic-app/basic-server:latest
+docker image push aneeshbhat/basic-server:latest
